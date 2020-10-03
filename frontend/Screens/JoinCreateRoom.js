e65899c2477d9cb9900a29a446a763fd7f537a06
jacksonhack <56742154+jacksonhack@users.noreply.github.com>
2020-10-03 16:05:08 -0400

    iphone2

--- frontend/Screens/JoinCreateRoom.js
+++ frontend/Screens/JoinCreateRoom.js
@@ -1,5 +1,6 @@
 import React, { Component } from './../node_modules/react';
 import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { Button } from 'react-native';
 
 export default class JoinCreateRoom extends React.Component {
     constructor(props) {
@@ -10,7 +11,9 @@ export default class JoinCreateRoom extends React.Component {
     render() {
       return (
         <View style={styles.container}>
-            <Text>Join Create Room</Text>      
+            <Text>Join Create Room</Text>  
+            <Button title="Create Room" onPress = { () => {this.props.navigation.navigate('ToppingsSelect')}}></Button>    
+            <Button title="Join Room" onPress = { () => {this.props.navigation.navigate('JoinRoom')}}></Button>
         </View>
       );
     }
@@ -23,5 +26,10 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center', 
       backgroundColor: 'white'
+    },
+    buttons: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',    
     }
   });
--- frontend/Screens/JoinRoom.js
+++ frontend/Screens/JoinRoom.js
@@ -1,5 +1,6 @@
 import React, { Component } from './../node_modules/react';
 import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { Button } from 'react-native';
 
 export default class JoinRoom extends React.Component {
     constructor(props) {
@@ -10,7 +11,14 @@ export default class JoinRoom extends React.Component {
     render() {
       return (
         <View style={styles.container}>
-            <Text>Join Room</Text>      
+            <Text>Enter Room ID</Text> 
+            <TextInput
+              //style={signin_style.input}
+              placeholder="Code"
+              onChangeText={(code) => this.setState({code})}
+              value={this.state.code}
+          />    
+          <Button title="GO" onPress = { () => {this.props.navigation.navigate('ToppingsSelect')}}></Button> 
         </View>
       );
     }
